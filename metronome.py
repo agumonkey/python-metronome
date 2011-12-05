4c55a7ce5b43c816c145571f3744190a7474a44a
piotr.stankiewicz@gmail.com <piotr.stankiewicz@gmail.com@678e7d2e-3c5c-4e6a-8fdf-d50d69a685b8>
2011-12-05 19:07:09 +0000

    license info added
    git-svn-id: http://python-metronome.googlecode.com/svn/trunk@5 678e7d2e-3c5c-4e6a-8fdf-d50d69a685b8

--- metronome.py
+++ metronome.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
+# metronome.py - a programable metronome written in python
+# Copyright (C) 2011 Piotr Stankiewicz <piotr[dot]stankiewicz{at}gmail(dot).com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import time
 import sys
@@ -225,6 +240,10 @@ def usage():
                                 "yes" if metronome.live_accent == 1 else "no", 
                                 "Infinite"))
 if __name__ == "__main__":
+    print("Metronome  Copyright (C) 2011 Piotr Stankiewicz")
+    print("This program comes with ABSOLUTELY NO WARRANTY")
+    print("This is free software, and you are welcome to redistribute it")
+    print("under certain conditions.")
     metronome = Metronome()
     from_file = False
     try:
